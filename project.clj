637d27594560771b3c4a4b94910e2e7f69082100
Anton Panferov <smaant@gmail.com>
2017-07-13 09:54:43 -0700

    back to using 1.8

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [org.jooq/jooq "3.9.0"]]
-  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :profiles
   {:dev {:global-vars {*warn-on-reflection* false}
          :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
          :aliases {"test-all"
-                   ["with-profile" "dev,1.7" "test2junit"]}
+                   ["with-profile" "dev,1.8" "test2junit"]}
          :plugins [[lein-ancient "0.6.10"]
                    [test2junit "1.2.2"]]
          :dependencies [[org.postgresql/postgresql "9.4.1212"]
@@ -19,8 +19,7 @@
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   ;:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
 
   :java-source-paths ["src/java"])
 
